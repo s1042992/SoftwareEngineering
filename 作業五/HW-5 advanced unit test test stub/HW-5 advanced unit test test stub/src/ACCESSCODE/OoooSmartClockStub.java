903ba48db807f654a8be59b48f079f30228eca3b
s1042992 <40089665+s1042992@users.noreply.github.com>
2019-10-10 08:06:12 +0800

    Update stub

--- 作業五/HW-5 advanced unit test test stub/HW-5 advanced unit test test stub/src/ACCESSCODE/OoooSmartClockStub.java
+++ 作業五/HW-5 advanced unit test test stub/HW-5 advanced unit test test stub/src/ACCESSCODE/OoooSmartClockStub.java
@@ -7,21 +7,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
-class OoooSmartClockStub extends OoooSmartClock {
+class OoooSmartClockStub {
 	
+	OoooSmartClock testcase = new OoooSmartClock();
+	
+	//function to check if two value is equal.
 	void assertEquals(boolean expected, boolean actual) {
 		Assertions.assertEquals(expected, actual);
 	}
+	//function to check if the string is null.
+	boolean isEmpty(String value) {
+        if (value == null || value.trim().equals(""))
+            return true;
+        else
+            return false;
+    }
+	
 	@Test
 	void test() {
-		boolean b = Boolean.parseBoolean(getCurrentTimeStamp());
+		//System.out.println(testcase.getCurrentTimeStamp());
+		boolean b = isEmpty(testcase.getCurrentTimeStamp());
 	    boolean exp = true;
 		assertEquals(exp,b);
 	}
 	
 	@Test
 	void test2() {
-		boolean b = Boolean.parseBoolean(getLocalCurrentTimeStamp());
+		boolean b = isEmpty(testcase.getLocalCurrentTimeStamp());
 	    boolean exp = true;
 		assertEquals(exp,b);
 	}
